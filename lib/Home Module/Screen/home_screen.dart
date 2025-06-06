083d0824f10ea8b0e685c506433d11eb3424ed99
Nicolas18RO <nicorodrigueopayome@gmail.com>
2025-06-06 00:51:19 -0500

    Prueba 1: Uso de mapas: Fallo

--- lib/Home Module/Screen/home_screen.dart
+++ lib/Home Module/Screen/home_screen.dart
@@ -1,14 +1,26 @@
-import 'package:drive_me_application_v02/Componets/MyText.dart';
 import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  MapboxMap? mapboxMapController;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: MyText(texto: 'Home Screen', fontSizeText: 20),
+      body: Container(
+        child: MapWidget(
+          onMapCreated: (controller) {
+            setState(() {
+              mapboxMapController = controller;
+            });
+          },
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:drive_me_application_v02/Home%20Module/Screen/home_screen.dart';
 import 'package:drive_me_application_v02/Login%20Module/View/auth_screen.dart';
 import 'package:drive_me_application_v02/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 void main() async {
+  await setUp();
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -12,12 +17,22 @@ void main() async {
   runApp(const MyApp());
 }
 
+Future<void> setUp() async {
+  await dotenv.load(
+    fileName: '.env',
+  );
+  MapboxOptions.setAccessToken(
+    dotenv.env['MAPBOX_ACCESS_TOKEN']!,
+  );
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: AuthScreen());
+        debugShowCheckedModeBanner: false, home: HomeScreen() //AuthScreen()
+        );
   }
 }
